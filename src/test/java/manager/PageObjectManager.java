653b6a8291140a4e08780d578b2ee9d714a7f9f4
Twister <Twister@192.168.0.8>
2019-03-06 16:16:03 +0800

    *Add pageobjectmanager to manage pageobject creation

--- src/test/java/manager/PageObjectManager.java
+++ src/test/java/manager/PageObjectManager.java
@@ -1,5 +1,21 @@
 package manager;
 
+import org.openqa.selenium.WebDriver;
+
+import pageObject.LoginPage;
+
 public class PageObjectManager {
 
+	private WebDriver driver;
+	private LoginPage loginpage;
+
+	public PageObjectManager(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	public LoginPage loginpage()
+	{
+		return (loginpage==null) ? new LoginPage(driver) : loginpage;
+	}
 }
